0e848e6dbe38e431515c380628e5aff6bb1b1d32
abdurrahmanabid <abdurrahmanabid33@gmail.com>
2025-07-04 11:53:33 +0600

    add bottom tab, setup nativewind

---
+++ src/constants/icons.ts
@@ -0,0 +1,19 @@
+import arrow from "@/src/assets/icons/arrow.png";
+import home from "@/src/assets/icons/home.png";
+import logo from "@/src/assets/icons/logo.png";
+import person from "@/src/assets/icons/person.png";
+import play from "@/src/assets/icons/play.png";
+import save from "@/src/assets/icons/save.png";
+import search from "@/src/assets/icons/search.png";
+import star from "@/src/assets/icons/star.png";
+
+export const icons = {
+  home,
+  search,
+  person,
+  logo,
+  save,
+  star,
+  play,
+  arrow,
+};
